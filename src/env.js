da5d8a440caf4f542eb4ed379b46a64437a9f8c6
Myrmex <dfusi@hotmail.com>
2022-05-23 19:37:55 +0200

    Env

--- src/env.js
+++ src/env.js
@@ -3,6 +3,6 @@
   window.__env = window.__env || {};
 
   // environment-dependent settings
-  window.__env.apiUrl = "https://6009.cophilab-cloud.ilc.cnr.it/api/";
+  window.__env.apiUrl = "http://localhost:60849/api/";
   window.__env.version = '1.1.5';
 })(this);
